fb7dab5f297d7ebdae5e0e974603b0492aa177d7
Alexander Campos <alejandrocampos2010@gmail.com>
2017-06-18 17:44:18 +0000

    changes to Gem file, gem file lock, app controller, views todo index and show

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,20 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        @todo_description ="Make the curriculum"
-        @todo_pomodoro_estimate=9
+        todo_id = params[:id]
+       # If user selects Coke
+       #todo_des = "You get a coke, congrats!"
+       if todo_id == '1'
+           @todo_description ="You get a coke Congrats!"
+        elsif todo_id == '2'
+            @todo_description= "You get a Pepsi Congrats"
+        else 
+            @todo_description = "You'll have an infinite source of water forever and be banned from Coke and Pepsi as well!"
+       end
+       #If user selects "I like pepsi"
+       #todo_description= "You like pepsi, Congrats!"
     end
 end
+
+#if statement creates a page apart from the original
+
