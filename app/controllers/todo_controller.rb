33917ca811c5fb912fa06b9d64ffcfa397cb10f6
Alexander Campos <alejandrocampos2010@gmail.com>
2017-06-18 22:33:30 +0000

    App models, views, db migrate, schema, test fixtures, models changed

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,17 +1,30 @@
 class TodoController < ApplicationController
     def index
     end
+    def new
+    end
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
     def show
+    @todo_description = Todo.find_by_id(params[:id]).description
+=begin    
+    @todo=Todo.find_by_id(params[:id]) example
         todo_id = params[:id]
-       # If user selects Coke
-       #todo_des = "You get a coke, congrats!"
-       if todo_id == '1'
-           @todo_description ="You get a coke Congrats!"
-        elsif todo_id == '2'
-            @todo_description= "You get a Pepsi Congrats"
-        else 
-            @todo_description = "You'll have an infinite source of water forever and be banned from Coke and Pepsi as well!"
-       end
+    If user selects Coke
+        todo_des = "You get a coke, congrats!"
+    if todo_id == '1'
+        @todo_description ="You get a coke Congrats!"
+    elsif todo_id == '2'
+        @todo_description= "You get a Pepsi Congrats"
+    else
+        @todo_description = "You'll have an infinite source of water forever and be banned from Coke and Pepsi as well!"
+    end
+=end
        #If user selects "I like pepsi"
        #todo_description= "You like pepsi, Congrats!"
     end
